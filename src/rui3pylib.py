b22ccab385f48a20043132e11c597b811385749a
danielemagnaterradev <185693512+danielemagnaterradev@users.noreply.github.com>
2026-05-17 22:56:37 +0200

    fixed some typos

--- src/rui3pylib.py
+++ src/rui3pylib.py
@@ -73,9 +73,9 @@ class RUI3node(serial.Serial):
     def tryClose(self):
         super().close()
 
-    ###################################
-    #########GENERAL COMMANDS##########
-    ###################################
+    #####################################
+    ######### GENERAL COMMANDS ##########
+    #####################################
 
     def attention(self):
         response, ok = checkSuccess(self, "AT")
@@ -153,26 +153,24 @@ class RUI3node(serial.Serial):
             mac = mac[:12]
         else:
             mac = mac.ljust(12)
-        response, ok checkSuccess(self, f"AT+BLEMAC={mac}")
+        response, ok = checkSuccess(self, f"AT+BLEMAC={mac}")
         return response
 
     def getBootVer(self):
-        response, ok = checkSuccess("AT+BOOTVER=?")
+        response, ok = checkSuccess(self, "AT+BOOTVER=?")
         return response
 
-    
-    ###################################
-    #########LOW POWER COMMANDS########
-    ###################################
-
+    ######################################
+    ######### LOW POWER COMMANDS #########
+    ######################################
 
     def atSleep(self, time: int):
         # time is in milliseconds
-        response, ok = checkSuccess(f"AT+SLEEP={time}")
+        response, ok = checkSuccess(self, f"AT+SLEEP={time}")
         return response
 
     def getLowPowerMode(self):
-        response, ok = checkSuccess("AT+LPM=?")
+        response, ok = checkSuccess(self, "AT+LPM=?")
         return response
 
     def setLowPowerMode(self, on: bool):
@@ -181,163 +179,157 @@ class RUI3node(serial.Serial):
         else:
             mode = 0
         response, ok = checkSuccess(self, f"AT+LPM={mode}")
-            
+        return response
 
-    ###################################
-    #########SERIAL AT COMMANDS########
-    ###################################
-
+    ######################################
+    ######### SERIAL AT COMMANDS #########
+    ######################################
 
     def lockSerial(self):
         # This method locks the serial port and it will require a password to access it
-        # The default password is 00000000 
+        # The default password is 00000000
         # You can change the password with the setPassword method
         response, ok = checkSuccess(self, "AT+LOCK")
-        print(f"Serial port {self.port} is now locked")
+        if ok:
+            print(f"Serial port {self.port} is now locked")
+        else:
+            return response
 
     def setPassword(self, password: str):
-        if len(password) < 1 || len(password) > 8:
+        if len(password) < 1 or len(password) > 8:
             print("Password must be between 1 and 8 characters of length")
         else:
             response, ok = checkSuccess(self, f"AT+PWORD={password}")
 
         return response
 
-    ###################################
-    #########BOOTLOADER COMMANDS#######
-    ###################################
+    #######################################
+    ######### BOOTLOADER COMMANDS #########
+    #######################################
 
     # These methods, with the exception of the first, one only work in boot mode
 
     def setBootMode(self):
-        # This method sets the device in boot mode 
+        # This method sets the device in boot mode
         # boot mode can be interrupted with the run method
         # AT_BUSY_ERROR is returned when the bootloader process is already running
-        response, ok = checkSuccess("AT+BOOT")
+        response, ok = checkSuccess(self, "AT+BOOT")
         return response
 
     def getBootloadeVer(self):
-        response, ok = checkSuccess("AT+VER=?")
+        response, ok = checkSuccess(self, "AT+VER=?")
         return response
 
     def getBootloadeStatus(self):
-        response, ok = checkSuccess("AT+BOOTSTATUS")
+        response, ok = checkSuccess(self, "AT+BOOTSTATUS")
         return response
 
     def atRun(self):
         # This methods makes the device leave boot mode and boots into the application
-        response, ok = checkSuccess("AT+RUN")
+        response, ok = checkSuccess(self, "AT+RUN")
         return response
 
     def bootReset(self):
-        response, ok = checkSuccess("AT+RESET")
+        response, ok = checkSuccess(self, "AT+RESET")
         return response
 
     def bootUpdate(self):
         # Starts Y-modem receiving process
-        response, ok = checkSuccess("AT+UPDATE")
+        response, ok = checkSuccess(self, "AT+UPDATE")
         return response
 
-    ###################################
-    #########LORAWAN KEYS AND ID#######
-    ###################################
-
+    #######################################
+    ######### LORAWAN KEYS AND ID #########
+    #######################################
 
     def getDeviceEUI(self):
-        response, ok = checkSuccess("AT+DEVEUI=?")
+        response, ok = checkSuccess(self, "AT+DEVEUI=?")
         return response
 
     def setDeviceEui(self, deveui: str):
         # Check if the string is comprised of precisely 16 hexdigits
-        if all(char in string.hexdigits for char in deveui) && len(deveui) == 16:
-            response, ok = checkSuccess(f"AT+DEVEUI={eui}")
+        if all(char in string.hexdigits for char in deveui) and len(deveui) == 16:
+            response, ok = checkSuccess(self, f"AT+DEVEUI={deveui}")
             return response
         else:
             # String is malformed and will return AT_PARAM_ERROR
             logging.info("AT_PARAM_ERROR")
-            break
-    
+
     # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-    # The specifications and errors for the above method are valid for all the 
+    # The specifications and errors for the above method are valid for all the
     # methods in this section
 
     def getAppEUI(self):
-        response, ok = checkSuccess("AT+APPEUI=?")
+        response, ok = checkSuccess(self, "AT+APPEUI=?")
         return response
 
     def setAppEUI(self, appeui: str):
-        if all(char in string.hexdigits for char in appeui) && len(appeui) == 16:
-            response, ok = checkSuccess(f"AT+APPEUI={appeui}")
+        if all(char in string.hexdigits for char in appeui) and len(appeui) == 16:
+            response, ok = checkSuccess(self, f"AT+APPEUI={appeui}")
             return response
-        else
+        else:
             logging.info("AT_PARAM_ERROR")
-            break
 
     def getAppKey(self):
-        response, ok = checkSuccess("AT+APPKEY=?")
+        response, ok = checkSuccess(self, "AT+APPKEY=?")
         return response
 
     def setAppKey(self, appkey: str):
         # Must be 32 hexdigits
-        if all(char in string.hexdigits for char in appkey) && len(appkey) == 32:
-            response, ok = checkSuccess(f"AT+APPKEY={appkey}")
+        if all(char in string.hexdigits for char in appkey) and len(appkey) == 32:
+            response, ok = checkSuccess(self, f"AT+APPKEY={appkey}")
             return response
         else:
             logging.info("AT_PARAM_ERROR")
-            break
 
     def getDevAddr(self):
-        response, ok = checkSuccess("AT+DEVADDR=?")
+        response, ok = checkSuccess(self, "AT+DEVADDR=?")
         return response
 
     def setDevAddr(self, devaddr: str):
         # Must be 4 hexdigits
-        if all(char in string.hexdigits for char in devaddr) && len(devaddr) == 4:
-            response, ok = checkSuccess(f"AT+APPKEY={devaddr}")
+        if all(char in string.hexdigits for char in devaddr) and len(devaddr) == 4:
+            response, ok = checkSuccess(self, f"AT+APPKEY={devaddr}")
             return response
         else:
             logging.info("AT_PARAM_ERROR")
-            break
 
     def getAppSKey(self):
-        response, ok = checkSuccess("AT+APPSKEY=?")
+        response, ok = checkSuccess(self, "AT+APPSKEY=?")
         return response
 
     def setAppSKey(self, appskey: str):
         # Must be 32 hexdigits
-        if all(char in string.hexdigits for char in appskey) && len(appskey) == 32:
-            response, ok = checkSuccess(f"AT+APPSKEY={appskey}")
+        if all(char in string.hexdigits for char in appskey) and len(appskey) == 32:
+            response, ok = checkSuccess(self, f"AT+APPSKEY={appskey}")
             return response
         else:
             logging.info("AT_PARAM_ERROR")
-            break
 
     def getNetworkSKey(self):
-        response, ok = checkSuccess("AT+NWKSKEY=?")
+        response, ok = checkSuccess(self, "AT+NWKSKEY=?")
         return response
 
     def setNetworkSKey(self, netskey: str):
         # Must be 32 hexdigits
-        if all(char in string.hexdigits for char in netskey) && len(netskey) == 32:
-            response, ok = checkSuccess(f"AT+APPSKEY={netskey}")
+        if all(char in string.hexdigits for char in netskey) and len(netskey) == 32:
+            response, ok = checkSuccess(self, f"AT+APPSKEY={netskey}")
             return response
         else:
             logging.info("AT_PARAM_ERROR")
-            break
 
     def getNetworkID(self):
-        response, ok = checkSuccess("AT+NETID=?")
+        response, ok = checkSuccess(self, "AT+NETID=?")
         return response
 
-    def setNetworkID(self, netid: str)
+    def setNetworkID(self, netid: str):
         # Must be 6 hexdigits
-        if all(char in string.hexdigits for char in netid) && len(netid) == 6:
-            response, ok = checkSuccess(f"AT+APPSKEY={netid}")
+        if all(char in string.hexdigits for char in netid) and len(netid) == 6:
+            response, ok = checkSuccess(self, f"AT+APPSKEY={netid}")
             return response
         else:
             logging.info("AT_PARAM_ERROR")
-            break
 
     def getMulticastRootKey(self):
-        response, ok = checkSuccess("AT+MCROOTKEY=?")
+        response, ok = checkSuccess(self, "AT+MCROOTKEY=?")
         return response
