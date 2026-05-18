addc86d4d72fa381a9b6906cac16fdfc9e90e991
Daniele Magnaterra <185693512+danielemagnaterradev@users.noreply.github.com>
2026-05-18 11:43:36 +0200

    added two commands

--- src/rui3pylib.py
+++ src/rui3pylib.py
@@ -375,3 +375,28 @@ class RUI3node(serial.Serial):
         response, ok = checkSuccess(self, "AT+MCROOTKEY=?")
         if ok:
             return response
+
+
+    ###############################################
+    ######### LORAWAN JOINING AND SENDING #########
+    ###############################################
+
+
+    def setConfirmMode(self, on: bool):
+        # This method is used to configure the uplink payload to be confirmed or unconfirmed type
+        if on is True:
+            mode = 1
+        else:
+            mode = 0
+
+        response, ok = checkSuccess(self, "AT+CFM={mode}")
+        if ok:
+            return response
+
+    def getConfirmStatus(self):
+        # This command is used to access the status of the last SEND command
+        response, ok = checkSuccess(self, "AT+CFS=?")
+        if ok:
+                return response
+
+    
