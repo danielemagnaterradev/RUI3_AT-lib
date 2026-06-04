49905e4bf8f45209d7ed98d9e563c3000aa52439
danielemagnaterradev <185693512+danielemagnaterradev@users.noreply.github.com>
2026-06-04 10:04:18 +0200

    cleared example

--- src/example.py
+++ src/example.py
@@ -2,9 +2,8 @@ import rui3pylib
 
 iface = rui3pylib.RUI3Node()
 
-if iface.is_open:
-    iface.set_device_eui("AC1F09FFFE285584")
-    iface.get_device_alias()
-    iface.get_device_eui()
+iface.set_device_eui("AC1F09FFFE285584")
+iface.get_device_alias()
+iface.get_device_eui()
 
-    iface.close()
+iface.close()
