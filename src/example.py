0311b2d3db6ab9dfa20e6c2a71ed66260e7da7d4
danielemagnaterradev <185693512+danielemagnaterradev@users.noreply.github.com>
2026-06-04 10:22:20 +0200

    updated example.py

--- src/example.py
+++ src/example.py
@@ -1,9 +1,15 @@
 import rui3pylib
 
 iface = rui3pylib.RUI3Node()
+try:
 
-iface.set_device_eui("AC1F09FFFE285584")
-iface.get_device_alias()
-iface.get_device_eui()
+    iface.set_device_eui("AC1F09FFFE285584")
+    iface.get_device_alias()
+    iface.get_device_eui()
 
-iface.close()
+    iface.close()
+
+except rui3pylib.serial.serialutil.PortNotOpenError as e:
+    print(f"SerialException on {iface.port}: {e}")
+    if iface.is_open:
+        iface.close()
