fca7095dd4b0b9c1eed95282fbf0872bb99fcb6e
danielemagnaterradev <185693512+danielemagnaterradev@users.noreply.github.com>
2026-06-28 22:54:32 +0200

    realistic node provisioning script as an example

--- src/example.py
+++ src/example.py
@@ -1,15 +1,84 @@
-import rui3pylib
+import pandas as pd
+import rui3pylib # type: ignore
+import serial
+import sys
+import yaml
+from time import sleep
 
-iface = rui3pylib.RUI3Node()
-try:
 
-    iface.set_device_eui("AC1F09FFFE285584")
-    iface.get_device_alias()
-    iface.get_device_eui()
+def main():
+    node_number = 2
+    print("Insert device...")
+    try:
+        config = yaml.safe_load(open("lorawan.yaml"))
+        if config is not None:
+            app_config = config["app-config"]
+            device_config = config["device-config"]
+        else:
+            print("Error opening lorawan.yaml file")
+        node_list = pd.read_excel("config.xlsx")  # type: ignore
+        if node_list["deveui"][node_number] is None:
+            raise KeyError
 
-    iface.close()
+    except KeyboardInterrupt:
+        print("\nExiting...")
+        sleep(1)
+        sys.exit()
+    except KeyError:
+        print(f"node n.{node_number + 1} not found")
+        print("\nExiting...")
+        sleep(1)
+        sys.exit()
 
-except rui3pylib.serial.serialutil.PortNotOpenError as e:
-    print(f"SerialException on {iface.port}: {e}")
-    if iface.is_open:
-        iface.close()
+    while True:
+        try:
+            sleep(5)
+            node = rui3pylib.RUI3Node()  # type: ignore
+
+            if node.ping() is not None:  # pyright: ignore[reportUnknownMemberType]
+                rui3pylib.check_success(node, f"AT+PORT={device_config['port']}")  # type: ignore
+                rui3pylib.check_success(node, f"AT+SENDINT={device_config['sendint']}")  # type: ignore
+                node.set_device_eui(node_list["deveui"][node_number])  # type: ignore
+                node.set_app_eui(app_config["app-eui"])  # type: ignore
+                node.set_app_key(app_config["app-key"])  # type: ignore
+                node.set_network_join_mode(device_config["join-mode"])  # type: ignore
+                node.set_adaptive_rate(device_config["adr-enabled"])  # type: ignore
+                node.set_transmit_power(device_config["transmit-power"])  # type: ignore
+                node.set_data_rate(device_config["data-rate"])  # type: ignore
+                node.set_lorawan_class(device_config["lora-class"])  # type: ignore
+                node.set_confirm_mode(device_config["confirm-mode"])  # type: ignore
+                node.set_freq_band(device_config["lora-region-band"])  # type: ignore
+
+                while True:
+                    print("Please disconnect your device before continuing")
+                    prompt = input("Do you want to continue? (Y/N)")
+                    match prompt.upper():
+                        case "Y":
+                            node_number += 1
+                            print("Insert device...")
+                            break
+                        case "N":
+                            print("Exiting...")
+                            sleep(1)
+                            sys.exit()
+                        case _:
+                            print("Invalid input")
+                            continue
+                    break
+
+        except KeyboardInterrupt:
+            print("\nExiting...")
+            sleep(1)
+            sys.exit()
+        except serial.serialutil.PortNotOpenError:  # type: ignore
+            print("Device not found")
+            continue
+        except KeyError:
+            print(f"node n.{node_number + 1} not found")
+            print("\nExiting...")
+            sleep(1)
+            sys.exit()
+
+
+if __name__ == "__main__":
+    main()
